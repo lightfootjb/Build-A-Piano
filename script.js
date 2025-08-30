8b2b0e7db68769e5c8afd24a218d73dc9ce3a7e0
Jaidon <lightfootjb@vcu.edu>
2025-08-30 18:17:37 -0400

    features implemented

---
+++ script.js
@@ -0,0 +1,62 @@
+const volumeControl = document.getElementById('volume');
+
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
+// select all keys
+const keys = document.querySelectorAll('.key');
+
+//add an event list to all keys
+keys.forEach((key) => {
+  key.addEventListener('click', () => playNote(key));
+});
+
+// handlers
+function playNote(key) {
+    //get the note's letter value 
+    const noteAudio = document.getElementById(key.dataset.note);
+    
+    //reset the note's current time to 0
+    noteAudio.currentTime = 0;
+
+    // set volume from slider
+    noteAudio.volume = volumeControl.value;
+    
+    // play the note
+    noteAudio.play();
+    
+    //add a class, active, for styling purposes
+    key.classList.add('active');
+
+    // new note display feature
+    const noteDisplay = document.getElementById('note-display');
+    noteDisplay.textContent = `Note: ${key.dataset.note}`;
+    
+    // listen for when the sound has ended and remove active class
+    noteAudio.addEventListener('ended', () => {
+        key.classList.remove('active');
+      });
+  }
+
+  document.addEventListener('keydown', (e) => {
+    // Get the key that triggered the event
+    const key = e.key;
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+    const blackKeyIndex = BLACK_KEYS.indexOf(key);
+  
+    // If the key is being held down, don't play the note again
+    if (e.repeat) {
+        return;
+    }
+
+    if (whiteKeyIndex > -1) {
+      playNote(whiteKeys[whiteKeyIndex]);
+    }
+  
+    if (blackKeyIndex > -1) {
+      playNote(blackKeys[blackKeyIndex]);
+    }
+  });
